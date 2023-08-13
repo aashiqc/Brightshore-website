0c9b931e533275077d68502543b20563a9de255f
Ashiq <muhmdashiqc@gmail.com>
2023-08-13 10:17:56 +0530

    updated js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.querySelector('.toggle_btn');
+    const toggleBtnIcon = document.querySelector('.toggle_btn i');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
 
-toggleBtn.onclick = () => {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
+    toggleBtn.addEventListener('click', () => {
+        dropDownMenu.classList.toggle('open');
+        const isOpen = dropDownMenu.classList.contains('open');
 
-
-    toggleBtnIcon.classList = isOpen 
-    ? 'fa-solid fa-xmark'
-    : 'fa-solid fa-bars'
-}
+        toggleBtnIcon.className = isOpen 
+            ? 'fa-solid fa-xmark'
+            : 'fa-solid fa-bars';
+    });
+});
